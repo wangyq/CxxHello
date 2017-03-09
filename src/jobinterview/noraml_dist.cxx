5d1483ebabb008eae447ef0cbe3da9e317ab8602
Yinqing Wang <yinqingwang@gmail.com>
2017-03-09 09:36:40 +0800

    add normal distribution

---
+++ src/jobinterview/noraml_dist.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+
+/* ====================================
+ * 
+ *
+*/
+double heartlove(double mu1,double sigma1, double mu2,double sigma2)
+{
+    double value = 0.0f;
+
+    return value;
+}
+
