86d8fbb450b8a14160350c2bb31402520d11a34e
wangyq <yinqingwang@gmail.com>
2017-03-20 11:47:18 +0800

    accpted.

--- src/jobinterview/ali_largestnumber_string.cxx
+++ src/jobinterview/ali_largestnumber_string.cxx
@@ -33,32 +33,14 @@ using namespace std;
  *
  */
 
-string int2str(int v){
-	stringstream ss;
-	ss<<v;
-	return ss.str();
-}
-int str2int(string s){
-	int v;
-	stringstream ss;
-	ss<<s;
-	ss>>v;
-	return v;
-}
 
-struct LargeNum{
-	int num, rnum;
-	string numstr;
+class Solution {
+public:
 
-	int buildmaxringnum(string s){
-		char ch = '0' ; //max char
-		for(auto& c : s){
-			if( c > ch ) ch = c;
-		}
+	string findmaxringnum(string s){
 
 		string rstr = s;
 		for(unsigned int i = 0;i<s.length();i++){//find largest ring num!
-			if( s[i] == ch ){
 				string smid;
 				if( i< s.length()-1) {
 					smid = s.substr(i+1, s.length()-i-1);
@@ -73,63 +55,80 @@ struct LargeNum{
 				if( rstr.compare(s[i] + smid) <0 ){
 					rstr = s[i] + smid;
 				}
+		}//end of for!
+		return rstr; //get the largest ring num!
+	}
+	string findmaxringnumfromstartchar(string s1, string s2){ //start from s1, can split
+		string rstr = "";
+		unsigned int i = 0;
+		for( i=1;i<s1.length() && i<=s2.length();i++){
+			if( s1[i] >= s2[i-1] ) continue;
+			else{
+				rstr = s1.substr(0, i);
+				rstr += s2;
+				rstr += s1.substr(i, s1.length() - i);
+				break;
 			}
+		}//end of for
+		if( rstr.length() == 0 ){
+			rstr = s1 + s2;
 		}
-		return str2int(rstr); //get the largest ring num!
+		return rstr;//
 	}
-	LargeNum(int v){//construct function!
-		string s = int2str(v);
-		string s2 = s;
-		std::reverse(s2.begin(), s2.end());
-
-		int v2 = str2int(s2);
-		if( v < v2 ){
-			num = v2;
-			numstr = s2;
-		}else{
-			num = v;
-			numstr = s;
+	string findmaxringnum(string s1, string s2){ //start from s1, can split
+		string rstr = "";
+		for(unsigned int i = 0; i<s1.length();i++){
+			string smid , tmp;
+			if( i< s1.length()-1) {
+				smid = s1.substr(i+1, s1.length()-i-1);
+			}
+			if( i>0 ){
+				smid += s1.substr(0,i);
+			}
+			tmp = findmaxringnumfromstartchar(s1[i] + smid,s2);
+			if( rstr < tmp ) rstr = tmp;
+			std::reverse(smid.begin(),smid.end());
+			tmp = findmaxringnumfromstartchar(s1[i] + smid,s2);
+			if( rstr < tmp ) rstr = tmp;
 		}
-		rnum = buildmaxringnum(numstr);
+		return rstr; //
 	}
-}; // end of struct LargeNum!!
-
-class Solution {
-public:
-
+	bool isRingFirst(string s1, string s2){ //compare who is the first!
+		string ms1 = findmaxringnum(s1,s2);
+		string ms2 = findmaxringnum(s2,s1);
+		if( ms1 < ms2 ) return false;
+		return true;
+	}
+	/**
+	 *
+	 */
 	string buildLargestNumber(vector<int> & nums){
 
-		string largestnum;
-		int rnum = 0;
-		unsigned int i = 0;
-		vector<LargeNum> lnums;
+		vector<string> numstr;
+		for(auto& v: nums ){ //preprocessor for vector!
+			string s1 = std::to_string(v);
+			string s2 = string(s1.rbegin(),s1.rend());
 
-		for(i=0;i<nums.size();i++){
-			LargeNum l = LargeNum(nums[i]);
-			if( rnum < l.rnum ) {
-				rnum = l.rnum;
-			}
+			if( s1 < s2 ) numstr.push_back(s2); //
+			else numstr.push_back(s1); //
+		}
+		sort(numstr.begin(),numstr.end(),std::greater<string>());// desc order
+		if( nums.size() == 1 ){ //only one number!
+			return findmaxringnum(numstr[0]);
 		}
 
-		vector<int> vnums(nums.size()-1);
-		for(auto& ln : lnums){
-			if( rnum != ln.rnum ){
-				vnums.push_back(ln.num);
-			}
-		}
-		std::sort(vnums.begin(),vnums.end(),std::greater<int>()); //
-
-		string s1,s2;
-		for(auto& v : vnums){
-			s1 += int2str(v);
-		}
-		s2 = int2str(rnum);
-
-		for(i=0;i<s1.length() && i<s2.length();i++){
-
+		string rstr = "";
+		for(auto& v : numstr ){
+			if( isRingFirst(v, rstr) ) rstr = v; //find the first ring number!
 		}
 
-		return largestnum;
+		string midstr;
+		for(auto& v : numstr){
+			if( rstr.compare(v) == 0 ) continue;
+			midstr += v;
+		}
+
+		return findmaxringnum(rstr,midstr);
 	}
 
 
@@ -137,8 +136,10 @@ public:
 
 void start_ali_bignum_ring_string()
 {
-	int v = 719191913;
-	LargeNum n(v);
-	cout<<n.num<<" "<<n.numstr<<" "<<n.rnum<<endl;
+	Solution so;
+	vector<int> nums = {123,494,878,719191913};
+	//vector<int> nums = {92,28,19};
+	//vector<int> nums = {719191913};
+	cout<< so.buildLargestNumber(nums)<<endl;
 }
 
